2846fbd046f5b0a97d24414d02d1d179e779f57e
deeptanshu_gorai <deeptanshugorai2004@gmail.com>
2024-09-27 00:21:55 +0530

    #

--- WebApplication4/gridview.aspx
+++ WebApplication4/gridview.aspx
@@ -5,8 +5,25 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <link href="gridview.css" rel="stylesheet" />
 </head>
 <body>
-    
+    <header>
+        <div class="common">
+            <div>
+                <a href="#">Name Of Candidate</a>
+            </div>
+            <div>
+                <a href="#">Name of account holder</a>
+            </div>
+          </div>
+    </header>
+    <div class="body">
+        
+    </div>
+    <footer>
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+        <asp:Button ID="Button2" runat="server" Text="Button" />
+    </footer>
 </body>
 </html>
--- WebApplication4/gridview.aspx.designer.cs
+++ WebApplication4/gridview.aspx.designer.cs
@@ -13,5 +13,23 @@ namespace WebApplication4
 
     public partial class gridview
     {
+
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Button2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
     }
 }
